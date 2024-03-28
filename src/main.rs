29d2dfa48abbcffea2a1f6d9fcb7e231533d3e91
pseudobabble <harryjohnson500@gmail.com>
2024-03-28 17:27:01 +0000

    Add view video functionality to the cli

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use tokio::process::Child;
+use std::io::Write;
+use regex::Regex;
 use clap::{Command, Parser, Subcommand};
 use serde::{Serialize, Deserialize};
 use thirtyfour::prelude::*;
 use tokio;
+use reqwest;
 use serde_json::json;
 use serde_json::to_string_pretty;
+use std::error::Error;
+use reqwest::Url;
+use tempfile::Builder;
+use std::fs::File;
+use std::io::copy;
+
+fn sanitize_filename(filename: &str) -> String {
+    let reserved_chars = ['/', '?', '<', '>', '\\', ':', '*', '|', '"'];
+    filename
+        .chars()
+        .filter(|c| !reserved_chars.contains(c))
+        .collect()
+}
+
 
 /// Interacts with Muse
 #[derive(Parser)]
@@ -36,10 +53,20 @@ enum Commands {
 // TODO https://github.com/nabijaczleweli/termimage
 
 #[derive(Serialize, Deserialize, Debug)]
-struct VideoAttributes {
+struct VideoListItem {
     title: String,
     id: String,
     url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct VideoPage {
+    id: String,
+    title: String,
+    image: String,
+    channel: String,
+    url: String,
+    sidebar: Vec<VideoListItem>
 }
 
 
@@ -69,7 +96,7 @@ impl Muse {
     }
 
     // Perform a search operation using the stored WebDriver
-    pub async fn search(&mut self, query: &str) -> WebDriverResult<Vec<VideoAttributes>> {
+    pub async fn search(&mut self, query: &str) -> WebDriverResult<Vec<VideoListItem>> {
         // Ensure driver is initialized
 
         let formatted_query = query.replace(" ", "+");
@@ -93,7 +120,7 @@ impl Muse {
                          .to_string();
             let url = format!("https://www.youtube.com/watch?v={}", id);
 
-            video_attrs.push(VideoAttributes { title, id, url });
+            video_attrs.push(VideoListItem { title, id, url });
         }
         self.quit_driver().await;
 
@@ -128,8 +155,156 @@ impl Muse {
         }
     }
 
-    pub async fn view(video_url: &str) {
-        println!("Viewing '{}'", video_url);
+    async fn download_image_to_tmp(&self, url: &str) -> Result<(String, tempfile::TempDir), Box<dyn Error>> {
+        // Parse the URL and extract the file name
+        let parsed_url = Url::parse(url)?;
+        let path_segments = parsed_url.path_segments().ok_or("Cannot extract path segments")?;
+        let file_name = sanitize_filename(path_segments.last().ok_or("Cannot extract file name")?);
+        println!("got filename {}", file_name);
+
+        // Create a temporary file where the image will be saved
+        let dir = Builder::new().prefix("muse_image_download").tempdir()?;
+        let temp_file_path = dir.path().join(file_name);
+        println!("got tempdir");
+
+        // Use reqwest to download the image data
+        let response = reqwest::get(url).await?.error_for_status()?;
+        let mut dest = File::create(temp_file_path.clone())?;
+        println!("got request ");
+
+        // Use `std::io::copy` to copy the image data to the file
+        let content = response.bytes().await?;
+        dest.write_all(&content)?;
+        println!("saved file ");
+
+        // Return the path to the saved image as a String
+        Ok((temp_file_path.to_string_lossy().into_owned(), dir))
+    }
+
+    async fn get_ascii_from_image(&self, image_path: &str) -> Result<String, Box<dyn Error>> {
+        // Replace `your_application` with the name of the actual application
+        let output = std::process::Command::new("ascii-image-converter")
+            .arg(image_path)
+            .arg("-b")
+            .arg("--dither")
+            .args(&["-d", "30,13"])
+            .arg("-c")
+            .output()?;
+
+        if !output.status.success() {
+            let error_message = std::str::from_utf8(&output.stderr)?;
+            panic!("ascii-image-converter failed with: {}", error_message)
+        }
+        println!("got output ");
+
+        // Convert the captured stdout bytes to a UTF-8 encoded String
+        let result_string = std::str::from_utf8(&output.stdout)?.to_string();
+        println!("got ascii in function ");
+
+        Ok(result_string)
+    }
+
+    pub async fn view(&self, video_id: &str) -> WebDriverResult<VideoPage> {
+        let video_url = format!("https://www.youtube.com/watch?v={}", video_id);
+        let driver = self.driver.as_ref().expect("Driver not initialized");
+
+        driver.goto(video_url.clone()).await?;
+
+        println!("got to video page");
+        let title = driver.query(By::Css("h1.style-scope.ytd-watch-metadata"))
+                          .first()
+                          .await
+                          .unwrap()
+                          .wait_until()
+                          .displayed()
+                          .await
+            ;
+        let title = driver.find_element(
+            By::Css(
+                "h1.style-scope.ytd-watch-metadata > yt-formatted-string.style-scope.ytd-watch-metadata"
+            )
+        ).await.unwrap();
+        let title_text = title.text().await.unwrap();
+        println!("got title {}", title_text);
+
+        // Wait to ensure the page has loaded. Adjust the delay as necessary.
+        let channel = driver.query(By::Css("a.yt-simple-endpoint.style-scope.yt-formatted-string"))
+                          .first()
+                          .await
+                          .unwrap()
+                          .wait_until()
+                          .displayed()
+                          .await
+            ;
+        let channel = driver.find_element(
+            By::Css(
+                "a.yt-simple-endpoint.style-scope.yt-formatted-string"
+            )
+        ).await.unwrap();
+        let channel_text = channel.text().await.unwrap();
+        println!("got channel {}", channel_text);
+
+        let thumbnail_image = driver.query(By::Css(".ytp-cued-thumbnail-overlay-image"))
+                                    .first()
+                                    .await
+                                    .unwrap()
+                                    .wait_until()
+                                    .displayed()
+                                    .await
+            ;
+        let thumbnail_style_attr = driver.find_element(
+            By::Css(
+                ".ytp-cued-thumbnail-overlay-image"
+            )
+        ).await.unwrap().attr("style").await.expect("Style attribute not found");
+        println!("got thumbnail {}", thumbnail_style_attr.clone().expect("no attr").clone());
+
+
+        let downloaded_file_path;
+        let tempdir;
+        // Using a regular expression to extract the URL from the style attribute
+        let re = Regex::new(r#"url\("([^"]+)"\)"#).unwrap();
+        if let Some(caps) = re.captures(&thumbnail_style_attr.clone().expect("no attr").clone()) {
+            let url = &caps[1];
+            (downloaded_file_path, tempdir) = self.download_image_to_tmp(url).await.unwrap();
+        } else {
+            (downloaded_file_path, tempdir) = self.download_image_to_tmp("https://upload.wikimedia.org/wikipedia/commons/4/4a/Youtube-.png").await.unwrap();
+        };
+        println!("filepath: {}", downloaded_file_path.clone());
+
+        let ascii_art = self.get_ascii_from_image(&downloaded_file_path).await.unwrap();
+        println!("got ascii:\n {}", ascii_art.clone());
+
+        let video_elements = driver.find_all(By::Css("ytd-compact-video-renderer.style-scope.ytd-watch-next-secondary-results-renderer")).await?;
+        let mut video_attrs = Vec::new();
+        for video_element in video_elements {
+            let title = video_element.find(By::Css("span#video-title")).await.unwrap().attr("title").await?.unwrap_or_default();
+            let href = video_element.find(By::Css("a.yt-simple-endpoint.style-scope.ytd-compact-video-renderer")).await.unwrap().attr("href").await?.unwrap_or_default();
+            let url = format!("https://www.youtube.com{}", href);
+            println!("title: {}", title.clone());
+            println!("href: {}", href.clone());
+
+
+            let id = href.split("v=").nth(1)
+                         .and_then(|v| v.split('&').next())
+                         .unwrap_or_default()
+                         .to_string();
+
+            video_attrs.push(VideoListItem { title, id, url });
+        }
+        println!("got related videos");
+
+        Ok(
+            VideoPage {
+                id: video_id.to_string(),
+                title: title_text,
+                channel: channel_text,
+                image: ascii_art,
+                url: video_url,
+                sidebar: video_attrs
+            }
+        )
+
     }
 }
 
@@ -154,11 +329,8 @@ async fn main() -> WebDriverResult<()> {
             })
         },
         Commands::View { id } => {
-            json!({
-                "command": "view",
-                "message": "View functionality is under construction.",
-                "id": id
-            })
+            let video_page = muse.view(&id).await.unwrap();
+            json!(video_page)
         },
     };
 
